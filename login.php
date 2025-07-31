096134caebe1041eb4a50c29e59a7c174a0d6981
harkoy <mys10052003@gmail.com>
2025-07-30 21:16:46 -0300

    Update login.php

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>✨ Celestial Chat - Iniciar Sesión</title>
+<title>✨ MERLIN</title>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 <link rel="stylesheet" href="assets/css/login.css">
 </head>
